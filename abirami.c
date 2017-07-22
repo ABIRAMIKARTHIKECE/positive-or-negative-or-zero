9f9ca2fce7037448d2932024905c36f8d007b437
ABIRAMIKARTHIKECE <30343715+ABIRAMIKARTHIKECE@users.noreply.github.com>
2017-07-22 16:57:40 +0530

    Create abirami.c

---
+++ abirami.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n<0)
+printf("the given number is positive");
+ifelse(n>0)
+printf("the given number is negative");
+else
+printf("the given number is zero");
+}
+
